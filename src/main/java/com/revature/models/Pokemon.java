31e6fb075e4df179801f8d15420afbd2d04c9298
Steven Hanley <Steven.Hanley@revature.net>
2021-11-08 13:29:06 -0600

    ready for DOL

---
+++ src/main/java/com/revature/models/Pokemon.java
@@ -0,0 +1,92 @@
+package com.revature.models;
+
+import java.util.Objects;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Pokemon {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int pokeId;
+	private int pokedexId;
+	private String name;
+	private int baseXP;
+	@ManyToOne
+	@JoinColumn(name="trainerId")
+	private Trainer trainer;
+	public Pokemon() {
+		super();
+	}
+	public Pokemon(int pokedexId, String name, int baseXP, Trainer trainer) {
+		super();
+		this.pokedexId = pokedexId;
+		this.name = name;
+		this.baseXP = baseXP;
+		this.trainer = trainer;
+	}
+	public Pokemon(int pokeId, int pokedexId, String name, int baseXP, Trainer trainer) {
+		super();
+		this.pokeId = pokeId;
+		this.pokedexId = pokedexId;
+		this.name = name;
+		this.baseXP = baseXP;
+		this.trainer = trainer;
+	}
+	public int getPokeId() {
+		return pokeId;
+	}
+	public void setPokeId(int pokeId) {
+		this.pokeId = pokeId;
+	}
+	public int getPokedexId() {
+		return pokedexId;
+	}
+	public void setPokedexId(int pokedexId) {
+		this.pokedexId = pokedexId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getBaseXP() {
+		return baseXP;
+	}
+	public void setBaseXP(int baseXP) {
+		this.baseXP = baseXP;
+	}
+	public Trainer getTrainer() {
+		return trainer;
+	}
+	public void setTrainer(Trainer trainer) {
+		this.trainer = trainer;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(baseXP, name, pokeId, pokedexId, trainer);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pokemon other = (Pokemon) obj;
+		return baseXP == other.baseXP && Objects.equals(name, other.name) && pokeId == other.pokeId
+				&& pokedexId == other.pokedexId && Objects.equals(trainer, other.trainer);
+	}
+	@Override
+	public String toString() {
+		return "Pokemon [pokeId=" + pokeId + ", pokedexId=" + pokedexId + ", "
+				+ (name != null ? "name=" + name + ", " : "") + "baseXP=" + baseXP + ", "
+				+ (trainer != null ? "trainer=" + trainer : "") + "]";
+	}
+	
+	
+}
---
+++ src/main/java/com/revature/models/Trainer.java
@@ -0,0 +1,71 @@
+package com.revature.models;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+@Entity
+public class Trainer {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int trainerId;
+	private String name;
+	@OneToMany(mappedBy="pokeId")
+	private List<Pokemon> party;
+	public Trainer() {
+		super();
+	}
+	public Trainer(String name, List<Pokemon> party) {
+		super();
+		this.name = name;
+		this.party = party;
+	}
+	public Trainer(int trainerId, String name, List<Pokemon> party) {
+		super();
+		this.trainerId = trainerId;
+		this.name = name;
+		this.party = party;
+	}
+	public int getTrainerId() {
+		return trainerId;
+	}
+	public void setTrainerId(int trainerId) {
+		this.trainerId = trainerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Pokemon> getParty() {
+		return party;
+	}
+	public void setParty(List<Pokemon> party) {
+		this.party = party;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, party, trainerId);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Trainer other = (Trainer) obj;
+		return Objects.equals(name, other.name) && Objects.equals(party, other.party) && trainerId == other.trainerId;
+	}
+	@Override
+	public String toString() {
+		return "Trainer [trainerId=" + trainerId + ", " + (name != null ? "name=" + name + ", " : "")
+				+ (party != null ? "party=" + party : "") + "]";
+	}
+	
+	
+}
